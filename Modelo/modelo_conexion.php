cb040df289650dc6102e6c43ead824ea26c73709
Kinnouts <kinnouts@gmail.com>
2023-08-28 13:37:22 -0300

    carpeta js

--- Modelo/modelo_conexion.php
+++ Modelo/modelo_conexion.php
@@ -8,3 +8,19 @@ try{
 }catch(PDOException $e){
     echo 'Falló la conexión: '.$e->getMessage();
 }
+class conexionBD{
+    public function conexionPDO{
+        $host='localhost';
+        $usuario='root';
+        $contraseña='';
+        $dbName='mybqq';
+        try{
+            $pdo=new PDO("mysql:host=$host;dbname=$$dbname;",$usuario,$contraseña);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $pdo;
+       
+        }catch(Exception $e){
+            echo "La conexión ha fallado";
+        }
+    }
+}
